cb53fa989422f3431dde5289895e8139bce3e28d
AveryThompson7 <avery.thompson@mail.utoronto.ca>
2026-05-19 12:36:19 -0400

    Created Sankey diagram

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 let timeline = new Timeline("v1_timeline", "data/timeline_data_v1.csv");
+
+
+const sankey = new Sankey("data/timeline_data_v1.csv", "#sankey", {
+    width: 1100,
+    height: 420
+});
+
 timeline.initVis();
+sankey.initVis();
 
 /// Pre-rel: 4
 /// EXp: 9
